59d632fa18f62d076679e7443c6c429fc5d6f7db
devashish94 <roy.devashish94@gmail.com>
2023-12-02 05:03:45 +0530

    change day 1 part 1

--- Java/src/Day1/Trebuchet.java
+++ Java/src/Day1/Trebuchet.java
@@ -2,43 +2,27 @@ package Day1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Trebuchet {
     public static void main(String[] args) throws FileNotFoundException {
-        try {
-            File file = new File("src/Day1/input");
-            Scanner read = new Scanner(file);
-            int sum = 0;
-            while (read.hasNextLine()) {
-                String line = read.nextLine();
-                int firstNumber = getFirstNumber(line);
-                int lastNumber = getLastNumber(line);
-                sum += 10 * firstNumber + lastNumber;
-            }
-            System.out.println(sum);
-        } catch (FileNotFoundException e) {
-            System.out.println("file not found");
-            System.out.println(e.getMessage());
-            throw new FileNotFoundException();
-        }
-    }
+        File file = new File("src/Day1/input");
+        Scanner read = new Scanner(file);
 
-    private static int getLastNumber(String line) {
-        for (int i = line.length() - 1; i >= 0; i--) {
-            if (Character.isDigit(line.charAt(i))) {
-                return line.charAt(i) - '0';
-            }
-        }
-        return 0;
-    }
+        int sum = 0;
+        while (read.hasNextLine()) {
+            String line = read.nextLine();
 
-    private static int getFirstNumber(String line) {
-        for (char c : line.toCharArray()) {
-            if (Character.isDigit(c)) {
-                return c - '0';
+            List<Integer> arr = new LinkedList<>();
+            for (char c : line.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    arr.add(c - '0');
+                }
             }
+            sum += arr.get(0) * 10 + arr.get(arr.size() - 1);
         }
-        return 0;
+        System.out.println(sum);
     }
 }
